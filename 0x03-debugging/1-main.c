a18a1a5290f02a5253209b83513a93051ac27406
Okuta Chinenye <c.kesha91@gmail.com>
2023-04-28 18:19:48 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return 0
+ * Return: 0
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming : (\n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
