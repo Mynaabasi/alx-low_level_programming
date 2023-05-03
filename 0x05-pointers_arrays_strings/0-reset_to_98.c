276953ccb7bcd425150adc91b34d1eb73d7f559b
Okuta Chinenye <c.kesha91@gmail.com>
2023-05-03 12:08:04 -0700

    Pointing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - Placing a pointer at int to update its value.
- *
+ * updateValue - Placing a pointer at int to update its value.
+ * pointer - Showing deferencing.
  * Return: Always 0
  */
-void reset_to_98(int *n);
+void updateValue(int *pointer);
 {
-	int *n;
-	*n = 98;
+	*pointer = 98;
 	return (0);
 }
