fb898abed8f2cda1a684eb53ac221f6c714034f9
Okuta Chinenye <c.kesha91@gmail.com>
2023-05-03 11:49:57 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Placing a pointer at int to update its value.
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n);
+{
+	int *n;
+	*n = 98;
+	return (0);
+}
