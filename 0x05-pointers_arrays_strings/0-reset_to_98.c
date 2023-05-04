20ed417ee304c9d137a91dd128c5c0ac0a5fb86d
Okuta Chinenye <c.kesha91@gmail.com>
2023-05-04 03:39:53 -0700

    Pointing and Arraying

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * reset_to_98 - Write a function that takes a pointer to update its value
- * @n - an integer
- *
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
  * Return: Always 0.
  */
+
 void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("The value of n is now %p\n", &n);
-	return (0);
+	*n = 98;
 }
